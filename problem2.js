f9c3f81b454468015580361c32e893e64fd4eb16
raoatulyadav <atulyaadav99@gmail.com>
2023-02-05 01:51:57 +0530

    pushed file

---
+++ problem2.js
@@ -0,0 +1,18 @@
+let student1="luffy";
+
+let student2="zoro";
+
+let student3="nami";
+
+
+let student4="atul";
+
+
+console.log(student1,student2,student3,student4);//
+let company=["samsung","nokia","nokia"];
+console.log(company);//
+let classroom=["atul","rahul","rahul"];
+console.log(classroom["1"]);// //
+//
+let classroom =["atul","rahul","yogi"];
+classroom[1]="rahul";
